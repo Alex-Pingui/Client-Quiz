9a32edfa1b46359091c1d34aeeaf9133d2b6695c
AlexandreGuihard <guihardalex98@gmail.com>
2026-03-16 15:22:36 +0100

    Affichage de tous les questionnaires

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import Questionnaire from "./components/Questionnaire.vue";
+
+export default {
+  components: {Questionnaire},
+  data(){
+    return {
+      title: "Quiz",
+      questionnaires: []
+    };
+  },
+  mounted() {
+    let questionnaires=fetch("http://localhost:5000/quiz/api/v1.0/questionnaires", {headers: {"Content-Type": "application/json"}, method: "GET"});
+    questionnaires.then(
+        result => result.json()
+    ).then(
+        result => {
+          this.questionnaires = result["questionnaires"];
+          console.log(this.questionnaires);
+        }
+    ).catch(error => console.log(error));
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{title}}</h1>
+  <ul>
+    <Questionnaire :questionnaire="questionnaire" v-for="questionnaire in questionnaires"/>
+  </ul>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ client/src/components/Questionnaire.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  props: {
+    questionnaire: Object
+  }
+}
+</script>
+
+<template>
+  <li>
+    <span>
+      {{questionnaire.name}}
+      <button class="btn btn-default">Questions</button>
+    </span>
+    <div class="input-group">
+      <input placeholder="Modifier le nom du questionnaire"
+             type="text"
+             class="form-control">
+      <span class="input-group-btn">
+      <button class="btn btn-default"
+              type="button">Modifier
+      </button>
+    </span>
+    </div>
+    <input type="button"
+           class="btn btn-danger"
+           value="Supprimer">
+  </li>
+</template>
+
+<style scoped>
+
+</style>
